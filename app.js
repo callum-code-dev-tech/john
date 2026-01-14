72444064839949a399f12195feb4c40b10042d93
callum-code-dev-tech <callumwithington2006@gmail.com>
2026-01-14 11:08:40 +0000

    added js code

--- app.js
+++ app.js
@@ -1,1 +1,22 @@
-hi
+console.log(`hello, people`)
+
+function runsOtherFunctions(a){
+    console.log(a)
+}
+
+runsOtherFunctions(technology)
+
+function technology(){
+    console.log(`welcome to a new age`)
+}
+
+runsOtherFunctions(goodBye)
+
+function goodBye(){
+    console.log(`nice knowing ya`)
+}
+
+
+function notifyUser(){
+    console.log(`notifyUser hi hope this is working`)
+}
